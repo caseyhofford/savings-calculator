1741e4c37aa2c6cb0debc79a57a6b3d9d042fbd9
Casey Hofford <casey.hofford@gmail.com>
2019-11-21 09:30:19 -0800

    added calcSav()

--- script.js
+++ script.js
@@ -47,6 +47,148 @@ inputs.selectAll('option')
       .text(function(d) {return d.text});
 
 
+////////////////////
+//Variables
+/////////////////
+const lt_miles = 250000
+
+/////////////////////
+//diesel
+const cost_gas = 4.00
+
+//class 6
+const c6_diesel = {upfront: {
+    base : 70000,
+    reefer : 30000,
+    tow : 40000,
+    box : 10000
+  },
+  mpg: 10,
+  maintCPM:.2,
+  compCPM: .025,
+  vpd: 150,
+  inc_dpy: 0
+}
+
+//class 7
+const c7_diesel = {upfront:{
+    base : 100000,
+    reefer : 35000,
+    tow : 50000,
+    box : 10000
+  },
+  mpg: 7.5,
+  maintCPM:.25,
+  compCPM: .03,
+  vpd: 200,
+  inc_dpy: 0
+}
+
+//class 8
+const c8_diesel = {upfront:{
+    base:140000,
+    reefer : 0,
+    tow : 0,
+    box : 0
+  },
+  mpg:6.0,
+  maintCPM:.3,
+  compCPM: .04,
+  vpd: 300,
+  inc_dpy: 0
+}
+
+const diesel_vehicles = {6:c6_diesel,7:c7_diesel,8:c8_diesel}
+
+/////////////////////
+//electric
+const cost_kwh = .1
+const ev_savings = .5
+
+//class 6
+const c6_electric = {upfront: {
+    base : 90000,
+    reefer : 40000,
+    tow : 50000,
+    box : 10000
+  },
+  kwhm: 1,
+  maintCPM:((1-ev_savings)*c6_diesel.maintCPM),
+  compCPM: 0,
+  vpd: 150,
+  inc_dpy: 6
+}
+
+//class 7
+const c7_electric = {upfront:{
+    base : 100000,
+    reefer : 35000,
+    tow : 50000,
+    box : 10000
+  },
+  kwhm: 1.5,
+  maintCPM: ((1-ev_savings)*c7_diesel.maintCPM),
+  compCPM: 0,
+  vpd: 200,
+  inc_dpy: 6
+}
+
+//class 8
+const c8_electric = {upfront:{
+    base:140000,
+    reefer : null,
+    tow : null,
+    box : null
+  },
+  kwhm: 2,
+  maintCPM: ((1-ev_savings)*c8_diesel.maintCPM),
+  compCPM: 0,
+  vpd: 300,
+  inc_dpy: 6
+}
+
+const electric_vehicles = {6:c6_electric,7:c7_electric,8:c8_electric}
+
+function calcVehicle(vehicle,reefer,tow,box) {
+  total_upfront = vehicle.upfront.base
+  if (reefer) total_upfront += vehicle.upfront.reefer
+  if (tow) total_upfront += vehicle.upfront.tow
+  if (box) total_upfront += vehicle.upfront.box
+  if ('mpg' in vehicle){
+    fuel_spend = cost_gas*(lt_miles/vehicle.mpg)
+  }
+  else if ('kwhm' in vehicle) {
+    fuel_spend = lt_miles*cost_kwh*vehicle.kwhm
+  }
+  maint_spend = lt_miles*vehicle.maintCPM
+  comp_spend = lt_miles*vehicle.compCPM
+  inc_up_total = -(vehicle.vpd*vehicle.inc_dpy*10)
+
+  lt_spend = total_upfront + fuel_spend + maint_spend + comp_spend + inc_up_total
+  cpm = (fuel_spend + maint_spend + comp_spend)/lt_miles
+  return {lt_spend:lt_spend,cpm:cpm}
+}
+
+//pulls out the values of each field and recalculates savings
+function calcSav(){
+  vehicle_class = d3.select("#vehicleclass").property("value")
+  reefer = d3.select("#reefer").property("value")
+  tow = d3.select("#tow").property("value")
+  box = d3.select("#box").property("value")
+  electric_totals = calcVehicle(electric_vehicles[vehicle_class],reefer,tow,box)
+  diesel_totals = calcVehicle(diesel_vehicles[vehicle_class],reefer,tow,box)
+  total_sav_d = diesel_totals.lt_spend - electric_totals.lt_spend
+  total_sav_p = total_sav_d/diesel_totals.lt_spend
+  sav_dpm = diesel_totals.cpm - electric_totals.cpm
+  sav_ppm = sav_dpm/diesel_totals.cpm
+  return {
+    total_sav_d:total_sav_d,
+    total_sav_p:total_sav_p,
+    sav_dpm:sav_dpm,
+    sav_ppm:sav_ppm
+  }
+}
+
 ///////////////////
 //Charts Output
 ////////////////
